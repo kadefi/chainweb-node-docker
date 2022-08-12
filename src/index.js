5b69228ab61d78e46ce2faefacae5da474f1b409
ST | Kadefi.money <st@kadefi.money>
2022-08-12 23:53:36 +0800

    refactor: only use one process

--- src/index.js
+++ src/index.js
@@ -15,31 +15,13 @@ app.use("/balance", indexRouter);
 const port = process.env.PORT || 3000;
 app.set("port", port);
 
-if (cluster.isPrimary) {
-  for (var i = 0; i < 4; i++) {
-    cluster.fork();
-  }
-
-  // If a worker dies, log it to the console and start another worker.
-  cluster.on("exit", function (worker, code, signal) {
-    console.log("Worker " + worker.process.pid + " died.");
-    cluster.fork();
-  });
-
-  // Log when a worker starts listening
-  cluster.on("listening", function (worker, address) {
-    console.log("Worker started with PID " + worker.process.pid + ".");
-  });
-} else {
-  const server = http.createServer(app);
-  server.listen(port);
-  server.on("listening", onListening);
-  function onListening() {
-    const addr = server.address();
-    const bind =
-      typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-    console.log("Listening on " + bind);
-  }
+const server = http.createServer(app);
+server.listen(port);
+server.on("listening", onListening);
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  console.log("Listening on " + bind);
 }
 
 export default app;

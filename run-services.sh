2cbcefb1bf5b04a6acefa01eca34364700b98a1f
ST | Kadefi.money <st@kadefi.money>
2022-08-12 23:53:36 +0800

    fix: shit what did I do

--- run-services.sh
+++ run-services.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# # Starting Chainweb Node
-# echo "Starting Chainweb Node"
-# test -d /data/chainweb-db/0 && ./run-chainweb-node.sh || (/chainweb/initialize-db.sh && ./run-chainweb-node.sh) &
+# Starting Chainweb Node
+echo "Starting Chainweb Node"
+test -d /data/chainweb-db/0 && ./run-chainweb-node.sh || (/chainweb/initialize-db.sh && ./run-chainweb-node.sh) &
 
 # Starting Pact DB API
 export NODE_ENV="production"
